8b85e15152ba52e2f2861348c8e96b216f141e1c
SwiftyOS <craigswift13@gmail.com>
2024-06-13 10:37:32 +0200

    Updated my config

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -5,6 +5,7 @@ return {
       require('trouble').setup {}
 
       vim.keymap.set('n', '<leader>tt', '<cmd>Trouble diagnostics toggle<cr>', { desc = '[T]oggle [T]rouble' })
+      vim.keymap.set('n', '<leader>td', '<cmd>Trouble todo toggle<cr>', { desc = 'Toggle [T]rouble To[D]o' })
 
       vim.keymap.set('n', '[t', function()
         require('trouble').next { skip_groups = true, jump = true }
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -49,9 +49,9 @@ return {
 
     -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<leader>Ds', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<leader>Di', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<leader>Do', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<leader>DO', dap.step_out, { desc = 'Debug: Step Out' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
@@ -80,7 +80,7 @@ return {
     }
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    vim.keymap.set('n', '<leader>Dl', dapui.toggle, { desc = 'Debug: See last session result.' })
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
