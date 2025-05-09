3fcf919c952be448fbd4e960f7d7a715954403b0
SwiftyOS <craigswift13@gmail.com>
2025-05-09 09:23:17 +0200

    updates

--- init.lua
+++ init.lua
@@ -727,7 +727,7 @@ require('lazy').setup({
   },
   {
     'mrcjkb/rustaceanvim',
-    version = '^4', -- Recommended
+    version = '^6', -- Recommended
     lazy = false, -- This plugin is already lazy
   },
 
