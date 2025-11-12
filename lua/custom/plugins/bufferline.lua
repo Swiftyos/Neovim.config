81f2a84b56c7ac92e16ea9b5a3dac1bbb2b56cbc
Swifty <craigswift13@gmail.com>
2025-11-12 15:43:02 +0100

    Added bufferline key maps and color fix

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -3,49 +3,27 @@ return {
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
+    -- Buffer navigation
+    vim.keymap.set('n', '<S-h>', ':bprevious<CR>', { desc = 'Previous buffer' })
+    vim.keymap.set('n', '<S-l>', ':bnext<CR>', { desc = 'Next buffer' })
+    vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = 'Close buffer' })
+    vim.keymap.set('n', '<leader>bs', ':BufferLinePick<CR>', { desc = 'Pick buffer' })
+
     require('bufferline').setup {
       options = {
         separator_style = 'slant',
-        -- LSP indicators
         diagnostics = 'nvim_lsp',
         diagnostics_indicator = function(count, level, diagnostics_dict, context)
           local icon = level:match 'error' and ' ' or ' '
           return ' ' .. icon .. count
         end,
-
-        -- Close icons
         show_close_icon = true,
         show_buffer_close_icons = true,
-
-        -- Tabpages
-        -- show_tab_indicators = true,
-
-        -- Hover events
         hover = {
           enabled = true,
           delay = 200,
           reveal = { 'close' },
         },
-        highlights = {
-          -- Customize slant colors
-          separator = {
-            fg = '#313244', -- Change this to your desired color
-            bg = '#1e1e2e',
-          },
-          separator_selected = {
-            fg = '#1e1e2e',
-            bg = '#1e1e2e',
-          },
-          separator_visible = {
-            fg = '#1e1e2e',
-            bg = '#1e1e2e',
-          },
-          -- Background fill color
-          fill = {
-            bg = '#181825', -- The background behind all tabs
-          },
-        },
-        -- Other useful options
         offsets = {
           {
             filetype = 'NvimTree',
@@ -56,5 +34,41 @@ return {
         },
       },
     }
+
+    -- Apply highlights after colorscheme loads
+    local function set_bufferline_colors()
+      local bg_inactive = '#1e1e2e' -- Background for inactive buffers
+      local bg_selected = '#313244' -- Background for selected buffer
+      local bg_fill = '#181825' -- Background behind all tabs
+
+      -- The key: separator fg should match the bg of the buffer it's attached to
+      vim.api.nvim_set_hl(0, 'BufferLineFill', { bg = bg_fill })
+
+      -- Inactive buffer separators
+      vim.api.nvim_set_hl(0, 'BufferLineSeparator', {
+        fg = bg_inactive, -- Match inactive buffer bg
+        bg = bg_fill,
+      })
+
+      -- Selected buffer separators
+      vim.api.nvim_set_hl(0, 'BufferLineSeparatorSelected', {
+        fg = bg_selected, -- Match selected buffer bg
+        bg = bg_fill,
+      })
+
+      -- Visible buffer separators (when you have splits)
+      vim.api.nvim_set_hl(0, 'BufferLineSeparatorVisible', {
+        fg = bg_inactive,
+        bg = bg_fill,
+      })
+    end
+
+    -- Set colors now
+    set_bufferline_colors()
+
+    -- Re-apply after colorscheme changes
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = set_bufferline_colors,
+    })
   end,
 }
